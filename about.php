41c9780d3b441b7bff1ad61f50e7acaeaacddd51
GlinCabije16 <angeline16cabije@gmail.com>
2025-12-22 10:38:53 +0800

    modify1

--- about.php
+++ about.php
@@ -30,7 +30,7 @@
 <nav>
   <a href="index.php">Home</a>
   <a href="about.php">About</a>
-  <a href="contact.php">Contact</a>
+  <a href="public_contact.php">Contact</a>
 </nav>
 
 <div class="container">
